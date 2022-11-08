cd5a9bbe5fac87ee70a99deab10abc36dc4874ac
JoaoBruno09 <1220256@isep.ipp.pt>
2022-11-08 00:36:29 +0000

    updated unit tests and Readme file, close #5

--- src/test/java/com/example/application/views/list/LoginFormTest.java
+++ src/test/java/com/example/application/views/list/LoginFormTest.java
@@ -14,15 +14,20 @@ public class LoginFormTest {
 
   @BeforeEach
   public void setupData() {
+    // give values to username and password
     username = "user";
     password = "userpass";
   }
 
   @Test
   public void loginFormTesting() {
+    // create new login form
     LoginForm formtest = new LoginForm();
+    // say to form that is going to be a login action
     formtest.setAction("login");
+    // create LoginEvent to pass to the form the username and password values
     LoginEvent form = new LoginEvent(formtest, false, this.username, this.password);
+    // Verify the user and password values if they are correct
     assertEquals("user", form.getUsername());
     assertEquals("userpass", form.getPassword());
   }
