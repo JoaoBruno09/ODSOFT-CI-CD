d6ad20eeca3ddedd4e26d1d844c120dfd2928f41
Gonçalo Pinho <goncalopinho@Goncalos-Air.lan>
2022-11-09 01:51:00 +0000

    Application.java now extends SpringBootServletInitializer so that can be deployed to Tomcat

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -6,7 +6,8 @@ import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 /**
  * The entry point of the Spring Boot application.
  */
@@ -15,7 +16,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Theme("flowcrmtutorial")
 @PWA(name = "VaadinCRM", shortName = "CRM", offlinePath = "offline.html",
         offlineResources = {"./images/offline.png"})
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
